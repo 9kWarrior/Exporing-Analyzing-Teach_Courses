e792388a3ce2f04f724a8c4f017cec6b7122eae4
9kWarrior <kik.lil.93@mail.ru>
2025-01-17 00:45:43 +0300

    Замена данных

--- moscow_tutors_analyze/first_step_clearing_data.py
+++ moscow_tutors_analyze/first_step_clearing_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import numpy as np
+from ast import literal_eval
 
 data = pd.read_csv(r"C:\Users\THUNDEROBOT\PycharmProjects\deeplom\repititors_price.csv")
 
@@ -13,10 +14,16 @@ data = pd.read_csv(r"C:\Users\THUNDEROBOT\PycharmProjects\deeplom\repititors_pri
 bad_columns = ['Reviews_number', 'Experience', 'Status', 'Location', 'Tags', 'Audience', 'Video_presentation', 'Photo']
 null_data = data[data.isnull().any(axis=1)]
 # print(null_data)
-
+null_data['Format'] = null_data['Format'].apply(lambda s: str(s).replace("at the tutor's","tutors place"))
+null_data['Format'] = null_data['Format'].apply(lambda s: str(s).replace("at the student's","students place"))
+for i in ['Categories', 'Format']:
+    null_data[i] = null_data[i].apply(lambda s: list(literal_eval(str(s))) if s != np.nan else s)
 df = pd.DataFrame(data)
 column_names = df.columns
 print(column_names)
 data_selected = df.drop(['Reviews_number', 'Experience', 'Status', 'Location', 'Tags', 'Audience', 'Video_presentation', 'Photo'], axis = 1)
 print(data_selected.shape)
 print(data_selected.head())
+
+null_expl = null_data.explode('Format')
+print(null_expl)
