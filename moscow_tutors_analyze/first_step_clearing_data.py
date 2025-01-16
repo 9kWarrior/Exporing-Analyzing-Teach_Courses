f4379f042db299ae6dfc39bd0a571530f2174023
9kWarrior <kik.lil.93@mail.ru>
2025-01-17 01:27:58 +0300

    Функция для замены аудиенции + доработки

--- moscow_tutors_analyze/first_step_clearing_data.py
+++ moscow_tutors_analyze/first_step_clearing_data.py
@@ -25,5 +25,27 @@ data_selected = df.drop(['Reviews_number', 'Experience', 'Status', 'Location', '
 print(data_selected.shape)
 print(data_selected.head())
 
-null_expl = null_data.explode('Format')
-print(null_expl)
+preproccessed_data = data.copy()
+preproccessed_data[['Reviews_number', 'Experience']] = preproccessed_data[['Reviews_number', 'Experience']].fillna(0)
+preproccessed_data['Status'] = preproccessed_data['Status'].fillna('-')
+preproccessed_data[['Location', 'Tags']] = preproccessed_data[['Location', 'Tags']].fillna('[]')
+preproccessed_data['Audience'] = preproccessed_data['Audience'].fillna('[\'All\']')
+preproccessed_data[['Video_presentation', 'Photo']] = preproccessed_data[['Video_presentation', 'Photo']].fillna('No')
+
+# print(preproccessed_data.isnull().sum().sum())
+
+cols_with_lists = ['Categories', 'Format', 'Location', 'Tags', 'Audience']
+preproccessed_data['Format'] = preproccessed_data['Format'].apply(lambda s: str(s).replace("at the tutor\'s", "tutors place"))
+preproccessed_data['Format'] = preproccessed_data['Format'].apply(lambda s: str(s).replace("at the student\'s", "students place"))
+preproccessed_data['Location'] = preproccessed_data['Location'].apply(lambda s: str(s).replace("[\'", '[\"'))
+preproccessed_data['Location'] = preproccessed_data['Location'].apply(lambda s: str(s).replace("\']", '\"]'))
+preproccessed_data['Location'] = preproccessed_data['Location'].apply(lambda s: str(s).replace("\', \'", '\", \"'))
+
+
+for i in cols_with_lists:
+    preproccessed_data[i] = preproccessed_data[i].apply(lambda s: list(literal_eval(str(s))) if s != np.nan else s)
+categories_series = preproccessed_data['Categories'].explode()
+# print(preproccessed_data)
+audience_series = preproccessed_data['Audience'].explode()
+audience_list = audience_series.unique()
+audience_list
