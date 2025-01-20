51db415e177b9ebec2406bb5287295f6b4c88c30
9kWarrior <kik.lil.93@mail.ru>
2025-01-20 04:51:52 +0300

    Saving for later options

--- moscow_tutors_analyze/first_step_clearing_data.py
+++ moscow_tutors_analyze/first_step_clearing_data.py
@@ -87,4 +87,4 @@ for i in ['Video_presentation', 'Photo']:
 for i in ['Status', 'Reviews_number', 'Experience']:
     preproccessed_data = preproccessed_data.astype({i: 'int64'})
 
-print(preproccessed_data.shape)
+preproccessed_data.to_csv('first_tutor_data_postprocesed.csv', index=False)
